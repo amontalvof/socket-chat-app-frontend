b6d79b4a0e1cf236a9cf884c2c4a7848414e2f63
amontalvof <amontalvof92@gmail.com>
2021-08-11 12:11:38 -0500

    feat: Check authentication status

--- src/auth/AuthContext.js
+++ src/auth/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useCallback, useState } from 'react';
-import { fetchWithoutToken } from '../helpers/fetch';
+import { fetchWithoutToken, fetchWithToken } from '../helpers/fetch';
 
 export const AuthContext = createContext();
 
@@ -55,7 +55,42 @@ export const AuthProvider = ({ children }) => {
         return resp.msg;
     };
 
-    const verifyToken = useCallback((params) => {}, []);
+    const verifyToken = useCallback(async () => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            setAuth({
+                uid: null,
+                checking: false,
+                logged: false,
+                name: null,
+                email: null,
+            });
+            return false;
+        }
+        const resp = await fetchWithToken({ endpoint: 'login/renew' });
+        if (resp.ok) {
+            localStorage.setItem('token', resp.token);
+            const { user } = resp;
+            setAuth({
+                uid: user.uid,
+                checking: false,
+                logged: true,
+                name: user.name,
+                email: user.email,
+            });
+            console.log('Authenticated!');
+            return true;
+        } else {
+            setAuth({
+                uid: null,
+                checking: false,
+                logged: false,
+                name: null,
+                email: null,
+            });
+            return false;
+        }
+    }, []);
 
     const logout = (params) => {};
 
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Route,
@@ -7,8 +7,23 @@ import {
 } from 'react-router-dom';
 import ChatPage from '../pages/ChatPage';
 import AuthRouter from './AuthRouter';
+import '../css/app.css';
+import { AuthContext } from '../auth/AuthContext';
 
 const AppRouter = () => {
+    const { auth, verifyToken } = useContext(AuthContext);
+
+    useEffect(() => {
+        verifyToken();
+    }, [verifyToken]);
+
+    if (auth.checking) {
+        return (
+            <div className="waitTextContainer">
+                <h1>Wait please ...</h1>
+            </div>
+        );
+    }
     return (
         <Router>
             <div>
