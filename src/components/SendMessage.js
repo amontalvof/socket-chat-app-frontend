bbb90e48357a9e9de49226bba2ec89fe1e2d52b6
amontalvof <amontalvof92@gmail.com>
2021-08-21 09:00:46 -0500

    feat: Send a message - Part 2

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { SocketContext } from '../context/SocketContext';
+import { AuthContext } from '../auth/AuthContext';
+import { ChatContext } from '../context/chat/chatContext';
 
 function SendMessage() {
+    const [message, setMessage] = useState('');
+    const { socket } = useContext(SocketContext);
+    const {
+        auth: { uid },
+    } = useContext(AuthContext);
+    const {
+        chatState: { chatActive },
+    } = useContext(ChatContext);
+
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (message.length === 0) return;
+        // TODO: emitir evento de socket
+        // {
+        //     from
+        //     to
+        //     message
+        // }
+        socket.emit('personal-message', {
+            from: uid,
+            to: chatActive,
+            message,
+        });
+        // TODO: Hacer el dispatch del mensaje
+        setMessage('');
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="type_msg row">
                 <div className="input_msg_write col-sm-9">
                     <input
                         type="text"
                         className="write_msg"
-                        placeholder="Mensaje..."
+                        placeholder="Message..."
+                        value={message}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="col-sm-3 text-center">
